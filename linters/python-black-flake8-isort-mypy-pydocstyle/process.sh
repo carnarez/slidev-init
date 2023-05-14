009d2e007c307577c9829350140c63b44b7c4989
carnarez <11967125+carnarez@users.noreply.github.com>
2023-05-14 14:14:00 +0200

    Added mypy

---
+++ linters/python-black-flake8-isort-mypy-pydocstyle/process.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+mypy_cache="/tmp/pycheck/${PWD##*/}/mypy_cache"
+
+if [ ! -d "$mypy_cache" ]; then mkdir --parents "$mypy_cache"; fi
+
+docker run --name pycheck \
+           --rm \
+           -u $(id -u):$(id -g) \
+           -v "$PWD":/usr/src \
+           -v "$mypy_cache":/tmp/mypy_cache \
+           -v /etc/group:/etc/group:ro \
+           -v /etc/passwd:/etc/passwd:ro \
+           -v /etc/shadow:/etc/shadow:ro \
+           linters/python \
+           "$@" \
+| grep -e 'Cannot find implementation or library stub for module' \
+       -e 'See https://mypy.readthedocs.io/en/stable/running_mypy.html#missing-imports' \
+       -v
--- linters/python-black-ruff-mypy/process.sh
+++ linters/python-black-ruff-mypy/process.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-mypy_cache="/tmp/pycheck/${PWD##*/}/mypy_cache"
+CACHE="/tmp/pycheck/${PWD##*/}/mypy_cache"
 
-if [ ! -d "$mypy_cache" ]; then mkdir --parents "$mypy_cache"; fi
+if [ ! -d "$CACHE" ]; then mkdir --parents "$CACHE"; fi
 
 docker run --name pycheck \
            --rm \
            -u $(id -u):$(id -g) \
            -v "$PWD":/usr/src \
-           -v "$mypy_cache":/tmp/mypy_cache \
+           -v "$CACHE":/tmp/mypy_cache \
            -v /etc/group:/etc/group:ro \
            -v /etc/passwd:/etc/passwd:ro \
            -v /etc/shadow:/etc/shadow:ro \
-           utils/lint-python \
-           "$@" \
+           linters/python \
+           "$@"
 | grep -e 'Cannot find implementation or library stub for module' \
        -e 'See https://mypy.readthedocs.io/en/stable/running_mypy.html#missing-imports' \
        -v
