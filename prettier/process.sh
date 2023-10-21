febcdf6a7afd8688d82328e88008451be902eedc
carnarez <11967125+carnarez@users.noreply.github.com>
2023-10-21 21:33:23 +0200

    Roll back `Prettier`

---
+++ prettier/process.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+docker run --name prettier \
+           --rm \
+           -u $(id -u):$(id -g) \
+           -v "$PWD":/usr/src \
+           -v /etc/group:/etc/group:ro \
+           -v /etc/passwd:/etc/passwd:ro \
+           -v /etc/shadow:/etc/shadow:ro \
+           utils/prettier \
+           "$@"
