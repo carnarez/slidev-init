a9bc111441171c96b00cdea40df586b885eb7e03
carnarez <11967125+carnarez@users.noreply.github.com>
2023-10-21 21:41:33 +0200

    Added a CSS linter

---
+++ linters/css/README.md
@@ -0,0 +1,4 @@
+**[`stylelint`](https://github.com/stylelint/stylelint) helper tool to lint CSS files.**
+Run `make env` to get everything set up (in `~/.local/bin`).
+
+Users and groups are mounted (read-only) from the host system to avoid ownership issues.
---
+++ linters/css/process.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+docker run --name csscheck \
+           --rm \
+           --tty \
+           --user $(id -u):$(id -g) \
+           --volume "$PWD":/usr/src \
+           --volume /etc/group:/etc/group:ro \
+           --volume /etc/passwd:/etc/passwd:ro \
+           --volume /etc/shadow:/etc/shadow:ro \
+           linters/css \
+           "$@"
